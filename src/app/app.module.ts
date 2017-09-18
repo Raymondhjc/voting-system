22073659b8b6b6dbd921735ff652e368a335bf15
lvergergsk <lvergergsl@outlook.com>
2017-09-18 06:35:55 -0400

    header added, modules added, angular-material configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component'
+import {AppComponent} from './app.component';
+import {AdminComponent} from './admin/admin.component';
+import {HeaderComponent} from './header/header.component'
+import {
+  MdButtonModule, MdCheckboxModule, MdDialogModule, MdGridListModule, MdInputModule, MdMenuModule,
+  MdToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdMenuModule,
+    MdGridListModule,
+    MdToolbarModule,
+    MdInputModule,
+    MdDialogModule
   ],
   providers: [],
   bootstrap: [
     AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  onHeaderSignIn() {
+  }
+}
