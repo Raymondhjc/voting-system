ce12fe61a4f771e46b527babd05b50964e703486
Raymondhjc <raymondhujc@gmail.com>
2017-09-24 17:50:38 -0400

    admin election search added

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 //for tables
 import { MdPaginator, MdSort } from '@angular/material';
@@ -8,8 +8,11 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
 
-//services needed
+//services required
 import { AdminService } from './admin.service'
 
 @Component({
@@ -27,12 +30,20 @@ export class AdminComponent implements OnInit {
   }
   displayedColumns = ['id', 'name', 'count'];
   elecTable: elecTableDataSource | null;
-  
+
   @ViewChild(MdPaginator) paginator: MdPaginator;
   @ViewChild(MdSort) sort: MdSort;
+  @ViewChild('filter') filter: ElementRef;
 
   ngOnInit() {
     this.elecTable = new elecTableDataSource(this.adminService, this.paginator, this.sort);
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.elecTable) { return; }
+        this.elecTable.filter = this.filter.nativeElement.value;
+      });
   }
 
 }
@@ -52,32 +63,54 @@ export class AdminComponent implements OnInit {
 // }
 
 export class elecTableDataSource extends DataSource<any> {
-  constructor(private adminService: AdminService, private _paginator: MdPaginator, private _sort: MdSort) {
+  constructor(private adminService: AdminService,
+    private _paginator: MdPaginator,
+    private _sort: MdSort) {
+
     super();
   }
+
+  rawData: any[] = [];
+  filteredData: any[] = [];
+  sortedData: any[] = [];
+
+  // Create filter BehaviorSubject and set getter and setter
+  _filterChange = new BehaviorSubject('');
+  get filter(): string { return this._filterChange.value; }
+  set filter(filter: string) { this._filterChange.next(filter); }
 
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<any[]> {
     const displayDataChanges = [
       this.adminService.dataChange,
       this._paginator.page,
-      this._sort.mdSortChange
+      this._sort.mdSortChange,
+      this._filterChange
     ];
     return Observable.merge(...displayDataChanges)
       .map(() => {
-        const rawData = this.adminService.data.slice();
-        const sortedData = this.getSortedData(rawData);
-        // Grab the page's slice of data.
+        this.rawData = this.adminService.data.slice();
+
+        // 1.Filter Data
+        this.filteredData = this.rawData.filter((item: any) => {
+          // Set Filter Type
+          let searchStr = (item.id + item.name).toLowerCase();
+          return searchStr.indexOf(this.filter.toLowerCase()) != -1;
+        });
+
+        // 2.Sort Filtered Data
+        this.sortedData = this.getSortedData(this.filteredData);
+
+        // 3. Pagination
         const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-        return sortedData.splice(startIndex, this._paginator.pageSize);
+        return this.sortedData.splice(startIndex, this._paginator.pageSize);
       });
   }
 
   disconnect() { }
 
-  //data sorting
-  getSortedData(data:any[]): any[] {
-    //const data = this.adminService.data.slice();
+  //Data Sorting
+  getSortedData(data: any[]): any[] {
     if (!this._sort.active || this._sort.direction == '') { return data; }
 
     return data.sort((a, b) => {
