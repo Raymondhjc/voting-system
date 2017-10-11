1d8b43dd81818203eb48d52e85a3f847c109cd70
tiangeB <tiangebu@gmail.com>
2017-10-10 22:08:00 -0400

    add count bar and achieve image slides
    store images in local and finish image slides, add count button and other related part.

--- src/app/scanner-page/scanner-page.component.ts
+++ src/app/scanner-page/scanner-page.component.ts
@@ -11,5 +11,28 @@ export class ScannerPageComponent implements OnInit {
 
   ngOnInit() {
   }
+  imageIndex: number = 0;
+  imageArray: string[] = ["assets/votingImage1.jpg","assets/votingImage2.jpg","assets/votingImage3.jpg"];
+ 
+  previousPage(image:any,previousButton:any,nextButton:any){
+    if (this.imageIndex > 0){
+      this.imageIndex = this.imageIndex - 1;
+      image.src = this.imageArray[this.imageIndex]
+    }
+    if (this.imageIndex == 0){
+      previousButton.disabled = true 
+    }
+    nextButton.disabled = false;
+  }
 
+  nextPage(image:any,previousButton:any,nextButton:any){
+    if (this.imageIndex < this.imageArray.length - 1){
+      this.imageIndex = this.imageIndex + 1;
+      image.src = this.imageArray[this.imageIndex]
+    }
+    if (this.imageIndex == this.imageArray.length - 1){
+      nextButton.disabled = true
+    }
+    previousButton.disabled = false;
+  }
 }
