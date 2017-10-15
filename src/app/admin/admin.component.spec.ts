d7fa062c30069995d36308f6d7f1919282e61940
Raymondhjc <raymondhujc@gmail.com>
2017-10-15 12:15:22 -0400

    add test

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,11 +1,62 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AdminComponent } from './admin.component';
+import { AdminService } from './admin.service';
+import { newElectionComponent } from './new-election/new-election.component';
+import { sectionComponent } from './new-election/section.component';
+//
+import {
+  MatInputModule,
+  MatSortModule,
+  MatButtonModule,
+  MatCardModule,
+  MatPaginatorModule,
+  MatTableModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatListModule,
+  MatExpansionModule,
+  MatRadioModule
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 describe('AdminComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        /** Election table modules */
+        MatTabsModule,
+        MatTableModule,
+        MatSortModule,
+        MatTabsModule,
+        MatPaginatorModule,
+        /** Create new election modules */
+        MatStepperModule,
+        ReactiveFormsModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatListModule,
+        MatExpansionModule,
+        MatRadioModule
+        
+    ],
       declarations: [
-        AdminComponent
+        AdminComponent,
+        newElectionComponent,
+        sectionComponent
       ],
+      providers: [
+        AdminService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +64,11 @@ describe('AdminComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should use the data from admin component service`, async(() => {
     const fixture = TestBed.createComponent(AdminComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AdminComponent);
+    let adminService = fixture.debugElement.injector.get(AdminService);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(adminService.data).toEqual(app.elecTable.rawData);
   }));
 });
