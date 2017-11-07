c2dba665409e5bc3c6fe6c448fee44f5fd70fbbd
YOULYU ZHANG <lvergergsk@outlook.com>
2017-11-07 14:59:33 -0500

    For merge.

--- e2e/admin.e2e-spec.ts
+++ e2e/admin.e2e-spec.ts
@@ -1,7 +1,7 @@
-import { AdminPage } from './admin.po';
-import { browser } from 'protractor'
+import {AdminPage} from './admin.po';
+import {browser} from 'protractor';
 
-fdescribe('hjc-voting-system App', () => {
+describe('hjc-voting-system App', () => {
   let page: AdminPage;
 
   beforeEach(() => {
@@ -37,7 +37,7 @@ fdescribe('hjc-voting-system App', () => {
 
   it('should generate a new election after valid inputs', () => {
     page.navigateTo();
-    //valid inputs of the first form
+    // valid inputs of the first form
     const firstForm = page.getFirstForm();
     page.getElectionNameInput().sendKeys('test election name');
     page.startDateCalendar().click();
@@ -47,7 +47,7 @@ fdescribe('hjc-voting-system App', () => {
     page.getendDate().click();
     browser.sleep(500);
 
-    //navigate to the second step
+    // navigate to the second step
     page.stepperButton(1).click();
     browser.sleep(1000);
     /**
@@ -60,26 +60,26 @@ fdescribe('hjc-voting-system App', () => {
     page.addSectionButton().click();
     page.sectionName(1).sendKeys('1.New Section 1');
     page.addOptionButton(1).click();
-    page.optionLabel(1,1).sendKeys('Option_1_of_Section_1');
-    
+    page.optionLabel(1, 1).sendKeys('Option_1_of_Section_1');
+
     page.addOptionButton(1).click();
-    page.optionLabel(1,2).sendKeys('Option_2_of_Section_1');
+    page.optionLabel(1, 2).sendKeys('Option_2_of_Section_1');
     browser.sleep(500);
 
-    //add another section, multichoice
+    // add another section, multichoice
     page.expansionPanel().click();
     page.optionType(2).click();
     browser.sleep(500);
     page.addSectionButton().click();
     page.sectionName(2).sendKeys('2.Sample Section Name');
     page.addOptionButton(2).click();
-    page.optionLabel(2,1).sendKeys('Sample option 1');
-    
+    page.optionLabel(2, 1).sendKeys('Sample option 1');
+
     page.addOptionButton(2).click();
-    page.optionLabel(2,2).sendKeys('Sample option 2');
+    page.optionLabel(2, 2).sendKeys('Sample option 2');
     browser.sleep(2000);
 
-    //navigate to the third step
+    // navigate to the third step
     page.stepperButton(2).click();
     browser.sleep(8000);
   });
