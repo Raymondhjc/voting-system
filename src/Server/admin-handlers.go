fca9872d60ddd5b0f6a989d413017174fb155640
raymondhujc@gmail.com <raymondhujc@gmail.com>
2017-11-27 15:39:36 -0500

    complete submiting new  election

--- src/Server/admin-handlers.go
+++ src/Server/admin-handlers.go
@@ -23,9 +23,20 @@ func addElectionHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("JSON ERROR:", err)
 	}
-	dberr := db.insertElection(ele)
+	dberr, endEleID, endQuID, endOpID := db.assignID()
+	dberr = db.insertElection(ele, endEleID)
+	for i := 0; i < len(ele.Questions); i++ {
+		qu := ele.Questions
+		endQuID = endQuID + 1
+		dberr = db.insertQuestion(qu, endQuID, endEleID, i);
+		for j := 0; j < len(qu[i].Options); j++ {
+			op := qu[i].Options
+			endOpID = endOpID + 1
+			dberr = db.insertOption(op, endOpID, endQuID, j);
+		}
+	}
 	if dberr != nil {
-		fmt.Println("db error:", dberr)
+		fmt.Println("DB ERROR:", dberr)
 	} else {
 		w.WriteHeader(http.StatusOK)
 	}
